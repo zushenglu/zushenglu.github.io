4d134bbbdbd5932123a55263feb07a1e00deb773
ZhanLu00 <zhanlu0523@gmail.com>
2021-01-20 13:50:24 -0600

    perlin noice demo
    made a easey demo based on perilin noice function (returns random value between 0 and 1). Its obvious that is could be used for terrin. but the future application is underdetermined

---
+++ terrain/sketch.js
@@ -0,0 +1,37 @@
+// Perlin Noise
+
+let rectHeights;
+let bikeLocation = 0
+let time = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectHeights = generateHeights();
+}
+
+function draw() {
+  background(220);
+  //let rectHeight = noise(time) * height;
+  
+  let howMany = width;
+  for (let i=bikeLocation; i<bikeLocation + howMany; i++) {
+    let rectWidth = width/howMany;
+    rect(rectWidth * (i - bikeLocation) ,height-rectHeights[i], rectWidth, rectHeights[i])
+}
+  if (keyIsPressed){
+    if (key === "d")
+    bikeLocation += 5;
+  }
+  if (key === "a"){
+    bikeLocation -= 5;
+  }
+}
+
+function generateHeights() {
+  let theHeights = [];
+  for (let i=0; i<5000; i++){
+    let rectHeight = noise(i*0.001) * height;
+    theHeights.push(rectHeight) ;
+  }
+  return theHeights;
+}
