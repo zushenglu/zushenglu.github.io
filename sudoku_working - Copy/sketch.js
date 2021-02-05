b8dbfa65ad45a84e660011d01aa223149c2848a9
Zushenglu1809 <Zushenglu1809@gmail.com>
2021-02-04 21:53:07 -0600

    finish the basic game
    it runs, shows like terms, ends itself when the game is completed, code improved, commented, answer grid added. ask if its enough or not. might consider adding background music and sorts

--- sudoku_working - Copy/sketch.js
+++ sudoku_working - Copy/sketch.js
@@ -1,8 +1,7 @@
 // Project Title
 
-let cellSideLength;
-let ROWS = 9;
-let COLS = 9;
+
+
 let grid = [[4,1,0,0,6,0,0,7,8],
   [7,0,3,5,0,1,4,2,0],
   [0,0,8,4,7,3,0,6,0],
@@ -23,26 +22,34 @@ let defaultGrid = [[4,1,0,0,6,0,0,7,8],
   [0,0,1,9,4,0,0,0,0],
   [0,4,9,0,2,8,0,0,0]];
 
-//createGrid(ROWS,COLS);
-let canvasSide;
-let selectedGrid;
-let lastgrid = [-1,-1];
+let answerGrid = [[4, 1, "5", "2", 6, "9", "3", 7, 8],
+  [7, "6", 3, 5, "8", 1, 4, 2, "9"],
+  ["9", "2", 8, 4, 7, 3, "5", 6, "1"],
+  ["1", 5, "7", "6", 9, 4, 8, 3, "2"],
+  [3, 9, "6", "8", 1, "2", 7, "4", "5"],
+  [2, 8, 4, 3, "5", "7", "1", "9", "6"],
+  [6, "7", "2", "1", "3", "5", "9", 8, "4"],
+  ["8", "3", 1, 9, 4, "6", "2", "5", "7"],
+  ["5", 4, 9, "7", 2, 8, "6", "1", "3"]]
+
+let canvasSide, cellSideLength;
+
+let keyEntered, selectedNumber;
 let ischosen = false;
-let inputNumber;
-let numberSet = [1,2,3,4,5,6,7,8,9];
-let isKeyInsert = false;
-let keyEntered;
-let rowNumbers;
+let selectedGrid = [0,0];
+let lastgrid = [-1,-1];
+
 let foundRepeat;
-let colNumbers;
+let startCheck = false;
 let verRepeat = false;
 let horRepeat = false;
-let squareNumber;
 let sqrRepeat = false;
-let startCheck = false;
-let selectedNumber;
+
+let finish = false;
 
 function setup() {
+
+  // makes the grid the biggest square of the screen
   if (windowWidth > windowHeight){
     createCanvas(windowHeight, windowHeight);
     cellSideLength = windowHeight/9;
@@ -53,54 +60,84 @@ function setup() {
     cellSideLength = windowWidth/9;
     canvasSide = windowWidth;
   }
-  selectedGrid = [0,0];
+
+  // makes the text appear in the center
   textAlign(CENTER);
-//  rectMode(CENTER)
+
 }
 
 function draw() {
-  background("green");
+
+  // draw grid, show number within the array, and check if the game
+  // game is completed
   drawGrid();
   displayNumber();
-
+  checkFinish();
 }
 
-function createGrid(ROWS,COLS){
-  let empty = [];
-  for (let y=0;y<ROWS;y++){
-    empty.push([]);
-    for (let x=0;x<COLS;x++){
-      empty[y].push();
+function checkFinish(){
+
+  // check if the the grid value is equal to the answer
+  finish = true;
+  for (let y=0;y<9;y++){
+    for (let x=0; x<9; x++){
+
+      if (grid[y][x] != answerGrid[y][x]){
+          finish = false;
+      }
     }
   }
-  return empty;
+
+  // shows end game screen
+  if (finish === true){
+    fill("#fbe6ff")
+    rect(0,0,width,height)
+    fill('#47d1d1')
+    text("GG", width/2, height/2)
+  }
 }
 
 function drawGrid(){
 
-  for (let y=0;y<ROWS;y++){
+  for (let y=0;y<9;y++){
+    
+    // draw horizontal split line
     if (y%3 === 0 && y !== 0){
       strokeWeight(10);
       line(0,y*cellSideLength,canvasSide,y*cellSideLength);
     }
-    for (let x=0;x<COLS;x++){
+    for (let x=0;x<9;x++){
+
+      // draw base grid
+      fill("white");
+      strokeWeight(1);
+      square(x*cellSideLength,y*cellSideLength,cellSideLength);
+
+      // use a and b to make code more readable
       let a = selectedGrid[0];
       let b = selectedGrid[1];
+
+      // darken the selected grid
       if (ischosen){
         fill("grey");
         strokeWeight(0);
         square(a*cellSideLength,b*cellSideLength,cellSideLength);
       }
+
+      // darken the grid with same number to help user solve the puzzle
+      if (grid[y][x] != 0 && grid[y][x] == selectedNumber){
+        fill("brown")
+        square(x*cellSideLength,y*cellSideLength,cellSideLength)
+      }
       
+      // highlight the grid when select a number that violates the sudoku rule
       if (foundRepeat){
         fill("red");
         strokeWeight(0);
         square(a*cellSideLength,b*cellSideLength,cellSideLength);
       }
-
-      fill("white");
-      strokeWeight(1);
-      square(x*cellSideLength,y*cellSideLength,cellSideLength);
+      
+      // draw verical split line
       if (x%3 === 0 && x!==0){
         strokeWeight(10);
         line(x*cellSideLength,0, x*cellSideLength, canvasSide);
@@ -110,8 +147,12 @@ function drawGrid(){
 }
 
 function displayNumber(){
-  for (let y=0;y<ROWS;y++){
-    for (let x=0;x<COLS;x++){
+  
+
+  for (let y=0;y<9;y++){
+    for (let x=0;x<9;x++){
+
+      // puts numbers in grid onto the screen, 0 excluded
       if (grid[y][x] !== 0){
         fill("black");
         textSize(cellSideLength*0.9);
@@ -121,38 +162,20 @@ function displayNumber(){
   }
 }
 
-function keyTyped(){
-
-  for (let i=0; i<9;i++){
-    if (key === i.toString()){
-      startCheck = false;
-      keyEntered = key;
-      if (ischosen){
-        if (defaultGrid[selectedGrid[1]][selectedGrid[0]] === 0 ){
-          
-          grid[selectedGrid[1]][selectedGrid[0]] = keyEntered;
-          console.log(grid[selectedGrid[1]][selectedGrid[0]]);
-          selectedNumber = keyEntered;
-          startCheck = true;
-          checkRepeat();
-        }
-      }
-    }
-    else if (key === " "){
-      keyEntered = key;
-      grid[selectedGrid[1]][selectedGrid[0]] = keyEntered;
-    }
-  }
-}
-
 function mouseClicked(){
 
+  // locate the grid that is clicked
   let x = Math.floor(mouseX/cellSideLength);
   let y = Math.floor(mouseY/cellSideLength);
   
+  // set grid location  and value as global value for repeat detection 
+  // and draw grid
   selectedGrid[0] = x;
   selectedGrid[1] = y;
+
   selectedNumber = grid[y][x];
+
+  // choose/ unchoose/ choose new grid
   if (x === lastgrid[0] && y === lastgrid[1]){
     ischosen = !ischosen;
   }
@@ -161,6 +184,10 @@ function mouseClicked(){
     foundRepeat = false;
   }
 
+  lastgrid[0] = x;
+  lastgrid[1] = y;
+
+  // control when to check for repeat
   if (ischosen === true){
     startCheck = true;
   }
@@ -168,13 +195,33 @@ function mouseClicked(){
     startCheck = false;
   }
 
-  lastgrid[0] = x;
-  lastgrid[1] = y;
-
   checkRepeat();
 }
 
+function checkRepeat(){
+
+  // check for repeat when a grid is chosen or when a value is altered
+  if (startCheck){
+ 
+    checkHorRepeat();
+    checkVerRepeat();
+    checkSquareRepeat();
+    
+    // if the number repeats in any of the test above, tell draw to
+    // highlight the grid
+    if (verRepeat || horRepeat || sqrRepeat){
+      foundRepeat = true;
+    }
+    else{
+      foundRepeat = false;
+    }
+    startCheck = false;
+  }
+}
+
 function checkHorRepeat(){
+
+  // check if the number in selected grid repeats in the same row
   let y = selectedGrid[1];
   horRepeat = false;
   for(let x=0;x<9;x++){
@@ -185,6 +232,8 @@ function checkHorRepeat(){
 }
 
 function checkVerRepeat(){
+
+  // check if the number in selected grid repeats in the same colomn
   let x = selectedGrid[0];
   verRepeat = false;
   for(let y=0;y<9;y++){
@@ -195,9 +244,12 @@ function checkVerRepeat(){
 }
 
 function checkSquareRepeat(){
+
+  // determine regional square the selected grid is in
   let x = Math.floor(selectedGrid[0]/ 3);
   let y = Math.floor(selectedGrid[1]/ 3);
 
+  // check if the number repeats in the same 3x3 square
   sqrRepeat = false;
   for (let i=0;i<3;i++){
     for (let j=0;j<3;j++){
@@ -208,20 +260,30 @@ function checkSquareRepeat(){
   }
 }
 
-function checkRepeat(){
-  if (startCheck){
- 
-    checkHorRepeat();
-    checkVerRepeat();
-    checkSquareRepeat();
-    
-    if (verRepeat || horRepeat || sqrRepeat){
-      console.log({verRepeat, horRepeat, sqrRepeat});
-      foundRepeat = true;
+function keyTyped(){
+
+
+  for (let i=1; i<10;i++){
+    // runs only when a number between 1-9 is typed
+    if (key == i){
+      keyEntered = key;
+
+      // check for repeat when a key between 1-9 is typed and a grid
+      // is selected
+      if (ischosen){
+        if (defaultGrid[selectedGrid[1]][selectedGrid[0]] === 0 ){
+          grid[selectedGrid[1]][selectedGrid[0]] = keyEntered;
+          selectedNumber = keyEntered;
+          startCheck = true;
+          checkRepeat();
+        }
+      }
     }
-    else{
-      foundRepeat = false;
+
+    // if the key pressed is space, it clears the value in the grid
+    else if (key === " " && defaultGrid[selectedGrid[1][selectedGrid[0]] != 0]){
+      keyEntered = key;
+      grid[selectedGrid[1]][selectedGrid[0]] = keyEntered;
     }
-    startCheck = false;
   }
 }
